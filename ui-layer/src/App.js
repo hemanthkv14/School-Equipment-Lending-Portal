f0598528223f93f645cdc0994bea5d818bc9cf8b
Monikachandra <monikachandrayadav@gmail.com>
2025-10-23 07:55:35 +0530

    Setup tailwind css

--- ui-layer/src/App.js
+++ ui-layer/src/App.js
@@ -1,6 +1,7 @@
 function App() {
   return (
-    <div className="App">
+     <div className="min-h-screen flex items-center justify-center bg-blue-500 text-white">
+      <div className="text-4xl font-bold">School equipment lending portal</div>
     </div>
   );
 }
