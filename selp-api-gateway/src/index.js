0b22eb0039d6e8a21d90ce5a06024c75026be32d
Pratheusha-kk <2024tm93723@wilp.bits-pilani.ac.in>
2025-11-03 09:06:58 +0530

    corrected the cors issue

--- selp-api-gateway/src/index.js
+++ selp-api-gateway/src/index.js
@@ -1,25 +1,41 @@
+const cors = require('cors');
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const path = require('path');
 const app = express();
 
+
 const AUTH_TARGET = 'http://localhost:4000';
 const CLIENT_TARGET = 'http://localhost:3000';
 const EQUIPMENT_TARGET = 'http://localhost:8080';
+
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
+}));
+
 
 // proxy /register* -> auth-service
 app.use('/api/auth', createProxyMiddleware({
     target: AUTH_TARGET,
     changeOrigin: true,
-    // CRITICAL: This strips '/api/auth' so 4000 receives only '/register'
-    pathRewrite: { '^/api/auth': '' }
+     pathRewrite: { '^/api/auth': '' } // removing /api/auth from the url
 }));
 
 app.use('/api', createProxyMiddleware({
-    target: EQUIPMENT_TARGET,
+    target: 'http://localhost:8080',
     changeOrigin: true,
-    pathRewrite: { '^/api': '' },
-  }));
+    pathRewrite: { '^': '/api' }   // prepend /api to everything after mount
+}));
+
+
+
+// app.use('/api', (req, res, next) => {
+//     console.log(`Gateway forwarding: ${req.method} ${req.originalUrl}`);
+//     next();
+// });
 
 // serve client (static) via proxy to client-service
 app.use('/', createProxyMiddleware({
